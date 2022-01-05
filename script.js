755434f8c05bcfc94108c93e8f139ab0ca4435dd
Nils Göbl <nils@goebl.cc>
2022-01-05 14:41:23 +0100

    Add basic UI functionality for playing the game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-  const option = Math.floor(Math.random()*3)
+  const option = Math.floor(Math.random() * 3)
   return option == 0 ? "rock" : option == 1 ? "paper" : "scissors"
 }
 
@@ -10,26 +10,39 @@ function capitalize(str) {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   const won = playerSelection === "paper" && computerSelection === "rock" ||
-  playerSelection === "rock" && computerSelection === "scissors" ||
-  playerSelection === "scissors" && computerSelection === "paper";
+    playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "paper";
   const draw = playerSelection === computerSelection;
   return draw ? "It's a draw!" : `You ${won ? "Win" : "Lose"}! ${capitalize(won ? playerSelection : computerSelection)} beats ${capitalize(!won ? playerSelection : computerSelection)}!`
 }
 
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-  for (i = 0; i < 5; i++) {
-    let playerInput = prompt("What do you pick?")
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0
+
+document.querySelectorAll("button.rps").forEach(button => {
+  button.addEventListener("click", e => {
+    const playerInput = e.target.getAttribute("id")
     const result = playRound(playerInput, computerPlay())
     if (result.startsWith("You W")) {
       playerWins++
     } else if (result.startsWith("You L")) {
       computerWins++
+    } else {
+      draws++
     }
-    console.log(result)
-  }
-  console.log(`Wins: ${playerWins}`)
-  console.log(`Losses: ${computerWins}`)
-  console.log(`Draws: ${5-playerWins-computerWins}`)
-}
+    let results = document.querySelector("#results")
+    results.innerHTML = `${result}<br>
+    <br>
+    Total Wins: ${playerWins}<br>
+    Total Losses: ${computerWins}<br>
+    Total Draws: ${draws}`
+
+    if (playerWins + computerWins + draws == 5) {
+      results.innerHTML += `<br><br>So the end result is a ${playerWins > computerWins ? "WIN" : computerWins > playerWins ? "LOSS" : "DRAW"}`
+      playerWins = 0
+      computerWins = 0
+      draws = 0
+    }
+  });
+});
